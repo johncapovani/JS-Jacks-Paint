e985d7e374bbe431637bb29960dc0ceb40aef208
John Capovani <john@Capovani.com>
2022-09-11 20:22:51 -0400

    worked with event listners to trigger other functions based on user action

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,71 +1,108 @@
+
+
+//This function loops through all img elements in the index.html sheet.
+//Adds two event listeners, a mouseover (add opacity) and a mouse out (remove opacity) 
+//addEventListener is considered a function
 function configureListeners() {
-    let images = // select img elements  
+    var images = document.getElementsByTagName('img');
 
-
-     for (var i = 0; i < images.length; i++) {        
-        // iterate over images and add mouseover event listeners      
-    } 
+    for (var i = 0; i < images.length; i++) {
+        document.getElementById(images[i].id).addEventListener('mouseover', addOpacity, false)
+        document.getElementById(images[i].id).addEventListener('mouseout', removeOpacity, false)
+    }
 }
 
+
+//When the event listner is triggered it calls this function
+///if this (the event that triggers it) it will add the class of dim to it
+//dim can be found on the css sheet which adds opacity to the element
+//Inokes the getProductInfo function by using the event.target.id as the partnumber value
 function addOpacity(event) {
-    // add appropriate CSS class
-    getProductInfo(event.target.id);     
+    if (!this.classList.contains('dim')) {
+        this.classList.add('dim')
+    }
+    getProductInfo(event.target.id);
 }
 
+//Removes the class dim from the object
+//removes the color-price text content and the color name content
 function removeOpacity(event) {
-     //remove appropriate CSS class
+    if (this.classList.contains('dim')) {
+        this.classList.remove('dim');
+    }
 
     let element = document.getElementById('color-price');
-        element.textContent = '';
-        
-    let color = document.getElementById('color-name');
-        color.textContent = ''; 
+    element.textContent = '';
 
-    event.preventDefault();    
+    let color = document.getElementById('color-name');
+    color.textContent = '';
+
+    //this tells the user that if the event does not get explicity handled its default action should not be taken as it normally would be
+    event.preventDefault();
 }
 
-function getProductInfo(paintColor) {
+
+//The partnumber is found from the id
+//This function is invoked by the add opacity function!
+function getProductInfo(partNumber) {
     let price;
-    let colorName;  
-    
-    switch (paintColor) {
-        case 'pn1':           
-            // set variables for price and color name and invoke a function to update the price     
-            break;           
+    let colorName;
+
+    switch (partNumber) {
+        case 'pn1':
+            price = '$19.99'
+            colorName = 'Lime Green'
+            updatePrice(colorName, price)
+            break;
         case 'pn2':
-            // set variables for price and color name and invoke a function to update the price    
-            break;            
+            price = '$12.99'
+            colorName = 'Medium Brown'
+            updatePrice(colorName, price)
+            break;
         case 'pn3':
-            // set variables for price and color name and invoke a function to update the price  
-            break;   
+            price = '$11.99'
+            colorName = 'Royal Blue'
+            updatePrice(colorName, price)
+            break;
         case 'pn4':
-            // set variables for price and color name and invoke a function to update the price  
-            break;   
+            price = '$14.99'
+            colorName = 'Bright Red'
+            updatePrice(colorName, price)
+            break;
         case 'pn5':
-            // set variables for price and color name and invoke a function to update the price       
-            break;   
+            price = '$9.99'
+            colorName = 'Solid White'
+            updatePrice(colorName, price)
+            break;
         case 'pn6':
-            // set variables for price and color name and invoke a function to update the price        
-            break;   
+            price = '$15.99'
+            colorName = 'Solid Black'
+            updatePrice(colorName, price)
+            break;
         case 'pn7':
-            // set variables for price and color name and invoke a function to update the price 
-            break;   
+            price = '$8.99'
+            colorName = 'Medium Blue'
+            updatePrice(colorName, price)
+            break;
         case 'pn8':
-            // set variables for price and color name and invoke a function to update the price   
-            break;   
+            price = '$16.99'
+            colorName = 'Light Purple'
+            updatePrice(colorName, price)
+            break;
         case 'pn9':
-            // set variables for price and color name and invoke a function to update the price 
-            break;   
-          default:              
+            price = '$17.99'
+            colorName = 'Bright Yellow'
+            updatePrice(colorName, price)
+            break;
+        default:
     }
 
-    function updatePrice(colorName, price)
-    {       
-        let colorPrice = // select element with corresponding id
-        // display price
-        
-        let color = // select element with corresponding id
-        //display color name
+    function updatePrice(colorName, price) {
+        let colorPrice = document.getElementById('color-price');
+        colorPrice.textContent = price;
+
+        let color = document.getElementById('color-name');
+        color.textContent = colorName;
     }
-    
+
 }
